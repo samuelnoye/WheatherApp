4a79af30a20d3180a0fc35ec9cc677e938f762dc
Samuel Noye <samuel.noye@amalitech.org>
2022-04-21 14:11:49 +0000

    Update Constants.swift

--- Constants.swift
+++ Constants.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+struct WeatherUrl {
+    private let baseUrl = "https://api.worldweatheronline.com/premium/v1/weather.ashx"
+}
 struct SkinType {
     let type1 = "Type 1 - Pale / Light"
     let type2 = "Type 2 - White / Fair"
@@ -15,7 +18,6 @@ struct SkinType {
     let type5 = "Type 5 - Dark Brown"
     let type6 = "Type 6 - Very Dark / Black"
 }
-
 struct defualtkeys {
     static let skinType = "skinType"
 }
