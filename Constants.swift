05969ac9a08908ebdcfcc3c66569e80a0f42dbc7
Samuel Noye <samuel.noye@amalitech.org>
2022-04-21 14:38:14 +0000

    Update Constants.swift

--- Constants.swift
+++ Constants.swift
@@ -9,7 +9,20 @@ import Foundation
 
 struct WeatherUrl {
     private let baseUrl = "https://api.worldweatheronline.com/premium/v1/weather.ashx"
+    private let key = "&key=9ff3d6d434ca4c9d8fb120254222104"
+    private let numDaysForecast = "&num_of_days=1"
+    private let format  = "&format=json"
     
+    private var coorStr = ""
+    
+    init (lat: String, long: String) {
+        self.coorStr = "?q=\(lat),\(long)"
+    }
+    
+    func getFullUrl () -> String {
+        return baseUrl + coorStr + key + numDaysForecast + format
+    }
+     
 }
 struct SkinType {
     let type1 = "Type 1 - Pale / Light"
