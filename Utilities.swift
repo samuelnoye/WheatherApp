8b8e6e9cd0139318b961e2adf887d77c193dd49d
Samuel Noye <samuel.noye@amalitech.org>
2022-04-21 10:13:59 +0000

    Initial commit

---
+++ Utilities.swift
@@ -0,0 +1,39 @@
+//
+//  Utilities.swift
+//  WheatherApp
+//
+//  Created by Samuel Noye on 20/04/2022.
+//
+
+import Foundation
+
+class Utilities {
+    func getStorage() -> UserDefaults {
+        return UserDefaults.standard
+    }
+    func setSkinType (value: String) {
+        let defualts = getStorage()
+        defualts.setValue(value, forKey: defualtkeys.skinType)
+        defualts.synchronize()
+    }
+    func getSkinType() -> String {
+        let defualt = getStorage()
+        if let result = defualt.string(forKey: defualtkeys.skinType){
+            return result
+        }
+            return SkinType().type1
+    }
+}
+
+struct SkinType {
+    let type1 = "Type 1 - Pale / Light"
+    let type2 = "Type 2 - White / Fair"
+    let type3 = "Type 3 - Mdeium / Light"
+    let type4 = "Type 4 - Olive Brown"
+    let type5 = "Type 5 - Dark Brown"
+    let type6 = "Type 6 - Very Dark / Black"
+}
+
+struct defualtkeys {
+    static let skinType = "skinType"
+}
---
+++ ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  WheatherApp
+//
+//  Created by Samuel Noye on 20/04/2022.
+//
+
+
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var skinLbl: UILabel!
+    var skintype = SkinType().type1 {
+        didSet{
+            skinLbl.text = "Skin: " + self.skintype
+        }
+    }
+    
+    let locationManager = CLLocationManager()
+    var coords = CLLocationCoordinate2D(latitude: 40, longitude: 40)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        skinLbl.text = "Skin: " + self.skintype
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Do any additional setup after loading the view.
+    
+    }
+    @IBAction func changeSkinBtn(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Skin Type", message: "Please choose skin type!", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: SkinType().type1, style: .default, handler: { (action) in
+            self.skintype = SkinType().type1
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type2, style: .default, handler: { (action) in
+            self.skintype = SkinType().type2
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type3, style: .default, handler: { (action) in
+            self.skintype = SkinType().type3
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type4, style: .default, handler: { (action) in
+            self.skintype = SkinType().type4
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type5, style: .default, handler: { (action) in
+            self.skintype = SkinType().type5
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type6, style: .default, handler: { (action) in
+            self.skintype = SkinType().type6
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Location changed")
+        if status == .authorizedWhenInUse{
+            getLocation()
+        }else if status == .denied {
+            let alert = UIAlertController(title: "Error", message: "Go to seettings and allow app to access your location! App can't function without it!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    func getLocation() {
+        if let loc = locationManager.location?.coordinate {
+            coords = loc
+        }
+        
+    }
+
+
+}
+
